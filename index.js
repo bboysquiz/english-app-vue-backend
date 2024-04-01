8e2341e14f1d9963b6641b614b1f02fa4cad4de7
bboysquiz <mrkiraller@gmail.com>
2024-04-01 11:34:17 +0300

    server settings

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const dictionaryRouter = require('./routes/dictionary.routes')
 const usersRouter = require('./routes/users.routes')
-const port = 3000;
+const port = process.env.PORT;
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  }));
 app.use(express.json())
 app.use('/api', dictionaryRouter)
 app.use('/api', usersRouter)
